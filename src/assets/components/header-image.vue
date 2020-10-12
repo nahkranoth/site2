c20c64494e6173192bebe50a76d24381a12a1c0d
nahkranoth <joey@nahkranoth.nl>
2020-10-13 01:31:07 +0200

    Added header image setup

--- src/assets/components/header-image.vue
+++ src/assets/components/header-image.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Header Image</div>
+  <div class="header-image">
+    <div class="image-container top"><img src="/src/assets/images/headerImage.jpg" alt=""></div>
+    <div class="image-container bottom"><img src="/src/assets/images/headerImage.jpg" alt=""></div>
+  </div>
 </template>
 
 <script>
@@ -15,6 +18,30 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  $side-margin: 14px;
+  $max-width: 1303px + ($side-margin*2);
 
+  .image-container{
+    overflow:hidden;
+    &.top{
+      width:200px;
+      height:400px;
+    }
+
+    &.bottom{
+      width:300px;
+      height:694px;
+      img{
+        position:relative;
+        top:-400px;
+      }
+    }
+  }
+
+  img{
+    width:1011px;
+    height:1094px;
+
+  }
 </style>
