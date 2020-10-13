f1457b4e107662c5feb02d710056c7c1f8ac966f
nahkranoth <joey@nahkranoth.nl>
2020-10-13 21:28:41 +0200

    Added text animation

--- src/assets/components/animated-copy.vue
+++ src/assets/components/animated-copy.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="animated-copy">
     <div class="top heroic-h th-red">
-
-      <span class="t-l">L</span>
+      <span class="t-l" ref="tl">L</span>
       <span class="heart-space"></span>
-      <span class="t-ve">V E</span>
-      <span class="t-dot">.</span>
+      <span class="t-ve" ref="tve">V E</span>
+      <span class="t-dot" ref="tdot">.</span>
+    </div>
+    <img class="heart-icon" ref="heart" src="src/assets/icons/heart.svg" alt="">
+
+
+    <div class="bottom heroic-h th-red">
+
+      <span class="d-l" ref="dl">L</span>
+      <span class="d-dot1" ref="ddotl">.</span>
+      <span class="d-a" ref="da">A</span>
+      <span class="d-dot1" ref="ddot2">.</span>
 
     </div>
-    <img class="heart-icon" src="src/assets/icons/heart.svg" alt="">
-    <div class="bottom heroic-h th-red">L.A.</div>
   </div>
 </template>
 
 <script>
+  import { gsap } from "gsap";
+
     export default {
-        name: "text-animation"
+        name: "text-animation",
+        mounted:function(){
+          this.fadeIn()
+        },
+      methods: {
+        fadeIn(){
+          //Top
+          gsap.fromTo(this.$refs.tl,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.heart,{top:60, autoAlpha:0},{top:30, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.tve,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.tdot,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+
+          //Bottom
+          gsap.fromTo(this.$refs.dl,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.ddot1,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.da,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+          gsap.fromTo(this.$refs.ddot2,{top:60, autoAlpha:0},{top:0, autoAlpha:1, duration: 0.79});
+        }
+      }
     }
 </script>
 
@@ -35,9 +62,10 @@
       width:355px;
       height:340px;
     }
-
+    span{
+      position:relative;
+    }
     .t-l{
-
     }
     .t-ve{
       letter-spacing: 17px;
@@ -49,7 +77,6 @@
 
   .heart-icon{
     position: absolute;
-    top: 29px;
     left: 140px;
     width: 385px;
     margin-right: 75px;
@@ -60,7 +87,9 @@
     top: -70px;
     display: inline;
     position: relative;
-    letter-spacing: 47px;
+    span{
+      position:relative;
+    }
   }
 
 </style>
